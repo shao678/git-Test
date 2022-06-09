bf9176bfc1cdd2cac7afb170e756b092de20e79e
wang <1581532657@qq.com>
2022-06-09 20:32:26 +0800

    second commit

--- src/main/java/com/good/git/gitTest.java
+++ src/main/java/com/good/git/gitTest.java
@@ -3,5 +3,6 @@ package com.good.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello hahaha");
     }
 }
